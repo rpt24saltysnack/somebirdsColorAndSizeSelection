804446a38953a733222bc246faf39c3aa4977f77
7MIMIRA <7mimira@gmail.com>
2020-11-17 00:55:10 -0500

    Refactor db.test.js & modify travis.yml file

--- test/db.test.js
+++ test/db.test.js
@@ -2,63 +2,45 @@ import { expect } from 'chai';
 const { Shoe, Color, Size, Quantity, Shoecolor, Shoesize } = require('../db/schema.js');
 
 describe('Database', () => {
-  it('has a shoes table with 99 records', () => {
-    Shoe.findAll({})
+  it('has a shoes table with 100 records', () => {
+    return Shoe.findAll({})
     .then(shoes => {
-      expect(shoes.length).to.equal(99);
+      expect(shoes.length).to.equal(100);
     })
-    .catch(err => {
-      console.error(err);
-    });
   });
 
   it('has a colors table with 25 records', () => {
-    Color.findAll({})
+    return Color.findAll({})
     .then(colors => {
       expect(colors.length).to.equal(25);
     })
-    .catch(err => {
-      console.error(err);
-    });
   });
 
   it('has a sizes table with 19 records', () => {
-    Size.findAll({})
+    return Size.findAll({})
     .then(sizes => {
       expect(sizes.length).to.equal(19);
     })
-    .catch(err => {
-      console.error(err);
-    });
   });
 
   it('has a quantities table with 32,500 records', () => {
-    Quantity.findAll({})
+    return Quantity.findAll({})
     .then(quantities => {
       expect(quantities.length).to.equal(32500);
     })
-    .catch(err => {
-      console.error(err);
-    });
   });
 
   it('has a shoecolors table with 2,500 records', () => {
-    Shoecolor.findAll({})
+    return Shoecolor.findAll({})
     .then(shoecolors => {
       expect(shoecolors.length).to.equal(2500);
     })
-    .catch(err => {
-      console.error(err);
-    });
   });
 
   it('has a shoesizes table with 1,300 records', () => {
-    Shoesize.findAll({})
+    return Shoesize.findAll({})
     .then(shoesizes => {
       expect(shoesizes.length).to.equal(1300);
     })
-    .catch(err => {
-      console.error(err);
-    });
   });
 });
