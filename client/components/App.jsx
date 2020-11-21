3a6a9c113b4d05c048f759955b284a3bb1552c08
7MIMIRA <7mimira@gmail.com>
2020-11-21 12:48:36 -0500

    Add App component
    Add App component renders the ColorPalette and SizeSelection components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import ColorPalette from './ColorPalette.jsx';
+import SizeSelection from './SizeSelection.jsx';
+import Axios from 'axios';
 
 function App(props) {
 
+  const [shoeID, setShoeID] = useState(props.shoeID);
+  const [classicColors, setClassicColors] = useState(['']);
+  const [limitedColors, setLimitedColors] = useState(['']);
+  const [sizes, setSizes] = useState(['']);
+
+  useEffect(() => {
+    Axios.get('/shoes/:shoeId/colors',{
+      params: {
+        shoeId: shoeID
+      }
+    })
+    .then(colors => {
+      setClassicColors(colors.data.filter(color => color.limited === false));
+      setLimitedColors(colors.data.filter(color => color.limited === true));
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }, [shoeID]);
+
+  useEffect(() => {
+    Axios.get('/shoes/:shoeId/sizes',{
+      params: {
+        shoeId: shoeID
+      }
+    })
+    .then(sizes => {
+      setSizes(sizes.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }, [shoeID]);
+
   return (
       <div className="app-container">
-        <h2>App Placeholder</h2>
+        <h2>App</h2>
+        <ColorPalette colors={ classicColors }/>
+        <ColorPalette colors={ limitedColors }/>
+        <SizeSelection />
       </div>
     );
 }
